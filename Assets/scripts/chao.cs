f217661c8f42473fd8c511bdd382e318c5ead3da
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-10 11:32:28 -0300

    Sistma de pontos

--- Assets/scripts/chao.cs
+++ Assets/scripts/chao.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 public class chao : MonoBehaviour
 {
     [SerializeField]
-    private float velocidade = 1.9f;
+    private float velocidade = 0.6f;
 
     private Vector3 posicaoInicial;
+
     private float tamanhoReal;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
         this.posicaoInicial = this.transform.position;
-        float tamanhoImagem = GetComponent<SpriteRenderer>().size.x; 
+        float tamanhoDaImagem = GetComponent<SpriteRenderer>().size.x;
         float escala = this.transform.localScale.x;
-        this.tamanhoReal = tamanhoImagem * escala;
-
+        tamanhoReal = tamanhoDaImagem * escala;
     }
 
-    // Update is called once per frame
     void Update()
     {
-         float deslocamento = Mathf.Repeat(this.velocidade * Time.time,tamanhoReal);
-        this.transform.position = this.posicaoInicial +Vector3.left * deslocamento;
+        float deslocamento = Mathf.Repeat(this.velocidade * Time.time, tamanhoReal);
+        this.transform.position = this.posicaoInicial + Vector3.left * deslocamento;
+
     }
 }
--- Assets/scripts/obstaculo.cs
+++ Assets/scripts/obstaculo.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class obstaculo : MonoBehaviour
 {
-    [SerializeField]
-    private float velocidade = 1.9f;
 
-    // Update is called once per frame
+
+    [SerializeField]
+    private float velocidade ;
+
+    [SerializeField]
+    private float variacaoY;
+
+    private Vector3 posicaopassaro;
+
+    private UIControler controladorUI;
+
+    private bool pontuei;
+
+    private void Awake()
+    {
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoY, variacaoY));
+    }
+
+    private void Start()
+    {
+        this.posicaopassaro = GameObject.FindObjectOfType<passaro>().transform.position;
+        this.controladorUI =  GameObject.FindObjectOfType<UIControler>();
+    }
+
     void Update()
     {
-        this.transform.Translate(Vector3.left*velocidade* Time.deltaTime);
+        this.transform.Translate(Vector3.left * this.velocidade * Time.deltaTime);
+        if(!this.pontuei && this.transform.position.x < this.posicaopassaro.x)
+        {
+            this.controladorUI.AdicionarPontos();
+            this.pontuei = true;
+        }
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        this.Destruir();
+    }
+
+    private void Destruir()
+    {
+        Destroy(this.gameObject);
     }
 }
