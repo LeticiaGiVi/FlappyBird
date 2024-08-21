dc5cdfc6a0d5bb4afb6b753910c21fcfd9c36fb0
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-21 09:44:25 -0300

    jogo

--- Assets/scripts/Tela_Start.cs
+++ Assets/scripts/Tela_Start.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour
+public class Tela_Start : MonoBehaviour
 {
     private UIDocument document;
     private Button botaoJogar;
+    private Button botaoMudar;
 
     void OnPlayGame(ClickEvent evt)
     {
         SceneManager.LoadScene("Main");
     }
+
+    void OnChangeSkin(ClickEvent evt)
+    {
+        SceneManager.LoadScene("Troca-personagem");
+    }
     private void Awake()
     {
         document = GetComponent<UIDocument>();
         botaoJogar = document.rootVisualElement.Q<Button>("start");
+        botaoMudar = document.rootVisualElement.Q<Button>("mudarPerssonagem");
         botaoJogar.RegisterCallback<ClickEvent>(OnPlayGame);
+        botaoMudar.RegisterCallback<ClickEvent>(OnChangeSkin);
     }
-    
 }
